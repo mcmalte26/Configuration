9a4139cb9a315b76fb4cbcf38c9879a53b0dbcf5
Joachim Jauß <joachim.jauss@outlook.de>
2018-09-17 17:04:35 +0200

    Because the configuration value 'list' of the 'testSection' is of type object (value property) the JsonConvert is unable to determin the correct type. Added some typeinformation will fix this. closes #1

--- Configuration/JsonConfiguration.cs
+++ Configuration/JsonConfiguration.cs
@@ -5,36 +5,43 @@ using System.IO;
 using System.Text;
 
 namespace Configuration {
-    public class JsonConfiguration : Configuration {
-        public JsonConfiguration(string fullpath) {
-            FullPath = fullpath;
-        }
-
-        public JsonConfiguration(string path, string fileName) {
-            FullPath = Path.Combine(path, fileName);
-        }
-
-        public string FullPath { get; }
-        
-        public void Save() {
-            string json = JsonConvert.SerializeObject(Sections,Formatting.Indented);
-            File.WriteAllText(FullPath, json, Encoding.UTF8);
-        }
-
-        public void Load() {
-            if (!File.Exists(FullPath)) {
-                File.Create(FullPath);
-            }
-                string jsonString = File.ReadAllText(FullPath, Encoding.UTF8);
-                List<Section> sections = JsonConvert.DeserializeObject<List<Section>>(jsonString);
-            if (sections != null) {
-                Sections.Clear();
-                Sections.AddRange(sections);
-            }else {
-                Sections.Clear();
-                //throw new FileLoadException("Loading failed because file is empty or does not exists.");
-            }
-        }
-
+  public class JsonConfiguration : Configuration {
+    public JsonConfiguration(string fullpath) {
+      FullPath = fullpath;
     }
+
+    public JsonConfiguration(string path, string fileName) {
+      FullPath = Path.Combine(path, fileName);
+    }
+
+    public string FullPath { get; }
+
+    public void Save() {
+      JsonSerializerSettings settings = new JsonSerializerSettings {
+        TypeNameHandling = TypeNameHandling.All
+      };
+      string json = JsonConvert.SerializeObject(Sections, Formatting.Indented, settings);
+      File.WriteAllText(FullPath, json, Encoding.UTF8);
+    }
+
+    public void Load() {
+      if (!File.Exists(FullPath)) {
+        File.Create(FullPath);
+      }
+      string jsonString = File.ReadAllText(FullPath, Encoding.UTF8);
+      JsonSerializerSettings settings = new JsonSerializerSettings {
+        TypeNameHandling = TypeNameHandling.All
+      };
+      List<Section> sections = JsonConvert.DeserializeObject<List<Section>>(jsonString, settings);
+      if (sections != null) {
+        Sections.Clear();
+        Sections.AddRange(sections);
+      }
+      else {
+        Sections.Clear();
+        //throw new FileLoadException("Loading failed because file is empty or does not exists.");
+      }
+    }
+
+  }
 }
